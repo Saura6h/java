92a402b156a1ac21d5b5919fe63595cb22a680a4
Saurabh Thakur <37576655+Saura6h@users.noreply.github.com>
2019-11-14 12:14:59 +0530

    Add files via upload

---
+++ progs/Fraction.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Fraction
+{
+	double num,denum;
+	Fraction(int a,int b)
+	{
+		num=a;
+		denum=b;
+	}
+
+	Fraction(double a, double b)
+	{
+		num=a;
+		denum=b;
+	}
+
+	void show()throws Exception
+	{
+		if(denum!=0)
+		{
+			if(num==0)
+				System.out.println("0");
+			else
+				System.out.println(num+"/"+denum);
+		}else{
+			throw new ArithmeticException("/ by zero");
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+			Fraction f1=new Fraction(5,3);
+			Fraction f2=new Fraction(5.2,3.2);
+			Fraction f3=new Fraction(5,0);
+			Fraction f4=new Fraction(0,3);
+		try{
+			f1.show();
+			f2.show();
+			f3.show();
+			f4.show();
+		}
+		catch(Exception e){
+			System.out.println("Denominator should not be zero");
+		}
+
+	}
+}
